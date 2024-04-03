58dfe3352fa33d376773827a364bd4a93cd208fe
Amanda Picinini <amandaferreirapicinini@gmail.com>
2024-04-03 16:32:51 -0300

    first commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Bruno Souza Picinini on 03/04/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
